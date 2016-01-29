dd4a21fdfefc31efde260471323b79954ec520e8
MoruaED <eduardo.morua@towasoftware.com>
2016-01-29 17:47:53 -0600

    agregue un form

---
+++ TW - Proyecto Ventas/TW - Proyecto Ventas/prueba de creacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TW___Proyecto_Ventas
+{
+    public partial class prueba_de_creacion : Form
+    {
+        public prueba_de_creacion()
+        {
+            InitializeComponent();
+        }
+    }
+}
