86f145c48a1bd56ab23d864e5bcdb1c277d9f530
Roman Alfaro <alromav1120@gmail.com>
2016-01-29 16:37:13 -0600

    Consultas

--- jajajaja/jajajaja/Form1.cs
+++ jajajaja/jajajaja/Form1.cs
@@ -12,9 +12,35 @@ namespace jajajaja
 {
     public partial class Form1 : Form
     {
+        private Label label1;
+    
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "JAJAJAJA";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
     }
 }
