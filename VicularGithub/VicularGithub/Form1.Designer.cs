1ae25b39ae8f3f76cb88e1349b7dc226462b5a3c
MoruaED <eduardo.morua@towasoftware.com>
2016-01-29 15:25:04 -0600

    xd

--- VicularGithub/VicularGithub/Form1.Designer.cs
+++ VicularGithub/VicularGithub/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(24, 104);
+            this.button1.Location = new System.Drawing.Point(103, 89);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(169, 86);
             this.button1.TabIndex = 0;
@@ -54,7 +54,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(33, 34);
+            this.button2.Location = new System.Drawing.Point(34, 189);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(220, 23);
             this.button2.TabIndex = 2;
